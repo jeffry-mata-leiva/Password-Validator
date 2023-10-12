614d53426630114420e5e0d7cfd081e822908d12
Jeffry Mata Leiva <jeffry.mata.leiva.itcr@gmail.com>
2023-10-12 11:34:46 -0600

    Add comments and update validation functions

--- PasswordValidator.js
+++ PasswordValidator.js
@@ -6,33 +6,64 @@ const validatePassword = password => {
         && validateSpecialCharacters(password);
 };
 
+/** Validates if the password meets with the length */
 const validateLength = password => {
     return password.length > 8;
 };
 
+/** Validates if the password includes at least one capital letter */
 const validateUpperCase = password => {
     const re = new RegExp("[A-Z]");
-    return re.test(password);
+    let count = 0;
+    password.split('').map( letter => {
+        if (re.test(letter)) {
+            count++;
+        }
+    });
+    return count > 0;
 };
 
+/** Validates if the password includes at least one lowercase letter */
 const validateLowerCase = password => {
     const re = new RegExp("[a-z]");
-    return re.test(password);
+    let count = 0;
+    password.split('').map( letter => {
+        if (re.test(letter)) {
+            count++;
+        }
+    });
+    return count > 0;
 };
 
+/** Validates if the password includes at least one number */
 const validateNumber = password => {
     const re = new RegExp("[0-9]");
-    return re.test(password);
+    let count = 0;
+    password.split('').map( letter => {
+        if (re.test(letter)) {
+            count++;
+        }
+    });
+    return count > 0;
 };
 
+/** Validates if the password includes an '_' (underscore character) */
 const validateSpecialCharacters = password => {
     const re = new RegExp("_");
-    return re.test(password);
+    let count = 0;
+    password.split('').map( letter => {
+        if (re.test(letter)) {
+            count++;
+        }
+    });
+    return count === 1;
 };
 
-console.log(`1. No length "Ab1_Ab1_": ${validatePassword("Ab1_Ab1_")}`);
+/** Test cases */
+console.log(`1. No length "Ab1_": ${validatePassword("Ab1_")}`);
 console.log(`2. No upper case "ab1_ab1_a": ${validatePassword("ab1_ab1_a")}`);
 console.log(`3. No lower case "AB1_AB1_C": ${validatePassword("AB1_AB1_C")}`);
 console.log(`4. No number "Abc_Abc_a": ${validatePassword("Abc_Abc_a")}`);
 console.log(`5. No special characters "Ab12Ab12a": ${validatePassword("Ab12Ab12a")}`);
-console.log(`6. Ok "Ab1_Ab1_a": ${validatePassword("Ab1_Ab1_a")}`);
+console.log(`6. More than one special characters "Ab1_Ab1_a": ${validatePassword("Ab1_Ab1_a")}`);
+console.log(`7. Ok "Ab1_cD2ef": ${validatePassword("Ab1_cD2ef")}`);
